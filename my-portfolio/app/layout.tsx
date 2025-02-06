0263214f3d0919edea73c7c6c711bad577922e2e
¨justineenyan¨ <enyanjustine146@gmail.com>
2025-02-06 08:33:44 +0000

    Continued with the previous commit message
    1. Created a new file constants/index.ts
    2. Added images in the public folder
    3. Modified the app/globals.css, app/layout.tsx, components/Navbar.tsx

--- my-portfolio/app/layout.tsx
+++ my-portfolio/app/layout.tsx
@@ -1,34 +1,29 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import Navbar from '@/components/Navbar'
+import Navigation from '@/components/Navigation'
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Portfolio-site",
-  description: "This is my portfolio website",
-};
+  title: 'Portfolio website',
+  description: 'This is my portfolio',
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={inter.className}>
+        <Navbar />
         {children}
-      </body>
+        <Navigation />
+        </body>
     </html>
-  );
+  )
 }
---
+++ my-portfolio/constants/index.ts
@@ -0,0 +1,120 @@
+
+import { RxHome, RxPerson, RxDashboard, RxClipboard } from "react-icons/rx";
+
+export const SkillData = [
+  {
+    name: "Html 5",
+    Image: "/html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Css",
+    Image: "/css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "JavaScript",
+    Image: "/js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    name: "Tailwind Css",
+    Image: "/tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "React",
+    Image: "/react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Redux",
+    Image: "/redux.png",
+    width: 80,
+    height: 80,
+  },
+
+  {
+    name: "TypeScript",
+    Image: "/ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Next js",
+    Image: "/next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Framer Motion",
+    Image: "/framer.png",
+    width: 80,
+    height: 80,
+  },
+];
+
+export const Socials = [
+  {
+    name: "Discord",
+    src: "/instagram.svg",
+  },
+  {
+    name: "Facebook",
+    src: "/facebook.svg",
+  },
+  {
+    name: "Instagram",
+    src: "/discord.svg",
+  },
+];
+export const Projects = [
+  {
+    title: "Pantry Tracker app",
+    text: "Keep track of your pantry items and get notified when they are about to expire.",
+    src: "/pan.png",
+  },
+  {
+    title: "AI Chatbot",
+    text: "have smart conversations based on user's input ",
+    src: "/ai.png",
+  },
+  {
+    title: "Quiz App",
+    text: "Quiz users on various topics and display their scores at the end.",
+    src: "/quiz.png",
+  },
+  {
+    title: "Password generator",
+    text: "Generate secure passwords for your accounts.",
+    src: "/Data_security_01.jpg",
+  },
+];
+
+export const NavLinks = [
+  {
+    name: "/",
+    icon: RxHome,
+    link: "/",
+  },
+  {
+    name: "/skills",
+    icon: RxPerson,
+    link: "/skills",
+  },
+  {
+    name: "/projects",
+    icon: RxDashboard,
+    link: "/projects",
+  },
+  {
+    name: "/contact",
+    icon: RxClipboard,
+    link: "/contact",
+  },
+];
